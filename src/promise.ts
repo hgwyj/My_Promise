a479027d8ac30c69915f0d6562d61fd72d79f6ee
huguang <huguang@lvzhoutech.com>
2019-12-31 17:41:38 +0800

    promise complete
    change promise.ts

--- src/promise.ts
+++ src/promise.ts
@@ -42,12 +42,106 @@ class MyPromise<R> implements Thenable<R> {
     private asap(cb: TimerHandler) {
         setTimeout(cb, 0);
     }
+    private getThen(value: any) {
+        try {
+            return value.then;
+        } catch (error) {
+            TRY_CATCH_ERROR.error = error;
+            return TRY_CATCH_ERROR;
+        }
+    }
     private mockResolve(value: any) {
         if (this === value) { this.mockReject(resolveSelfError); return; };
         if (!isObjectORFunction(value)) {
             this.fulfill(value);
             return;
         };
+        this.handleLikeThenable(value, this.getThen(value));
+    }
+    private handleLikeThenable(value: any, then: any) {
+        if (this.isThenable(value, then)) {
+            this.handleOwnThenable(value);
+            return;
+        }
+        // 获取 then 值失败且抛出异常，则以此异常为拒因 reject promise
+        if (then === TRY_CATCH_ERROR) {
+            this.mockReject(TRY_CATCH_ERROR.error);
+            TRY_CATCH_ERROR.error = null;
+            return;
+        }
+        // 如果 then 是函数，则检验 then 方法的合法性
+        if (isFunction(then)) {
+            this.handleForeignThenable(value, then);
+            return;
+        }
+        this.fulfill(value);
+    }
+    private tryThen(then: any, thenable: any, resolvePromise: any, rejectPromise: any) {
+        try {
+            then.call(thenable, resolvePromise, rejectPromise);
+        } catch (e) {
+            return e;
+        }
+    }
+    private handleForeignThenable(thenable: any, then: any) {
+        this.asap(() => {
+            // 如果 resolvePromise 和 rejectPromise 均被调用，
+            // 或者被同一参数调用了多次，则优先采用首次调用并忽略剩下的调用
+            // 此处 sealed (稳定否)，用于处理上诉逻辑
+            // sealed 参数 ---> 营造一个promise 环境状态一经改变就不能在进行更改
+            let sealed = false;
+            const error = this.tryThen(
+                then,
+                thenable,
+                (value: any) => {
+                    if (sealed) {
+                        return;
+                    }
+                    sealed = true;
+                    if (thenable !== value) {
+                        this.mockResolve(value);
+                    } else {
+                        this.fulfill(value);
+                    }
+                },
+                (reason: any) => {
+                    if (sealed) {
+                        return;
+                    }
+                    sealed = true;
+                    this.mockReject(reason);
+                }
+            );
+            if (!sealed && error) {
+                sealed = true;
+                this.mockReject(error);
+            }
+        });
+    }
+    private handleOwnThenable(thenable: any) {
+        // 处理 value 为 promise 对象的情况
+        const state = thenable.status;
+        const result = thenable.value;
+        if (state === PROMISE_STATUS.fulfilled) {
+            this.fulfill(result);
+            return;
+        }
+        if (state === PROMISE_STATUS.rejected) {
+            this.mockReject(result);
+            return;
+        }
+        this.subscribe(
+            thenable,
+            undefined,
+            (value: any) => this.mockResolve(value),
+            (reason: any) => this.mockReject(reason)
+        );
+    }
+    private isThenable(value: any, then: any) {
+        const sameConstructor = value.constructor === this.constructor;
+        const sameThen = then === this.then;
+        const sameResolve = value.constructor.resolve === MyPromise.resolve;
+        return sameConstructor && sameThen && sameResolve;;
     }
     private fulfill(value: any) {
         this.status = PromiseState.FULFILLED;
@@ -179,6 +273,54 @@ class MyPromise<R> implements Thenable<R> {
 
     finally(callback: any) {
         return this.then(callback, callback);
+    }
+
+    static resolve = (object: any) => {
+        //传入的参数是promise
+        if (object && typeof object === "object" && object.constructor === MyPromise) {
+            return object;
+        }
+        let promise = new MyPromise(() => { });
+        promise.mockResolve(object);
+        return promise;
+    }
+    static reject = (reason: any) => {
+        let promise = new MyPromise(() => { });
+        promise.mockReject(reason);
+        return promise;
+    }
+    static all = (entries: []) => {
+        let result: any[] = [];
+        let num = 0;
+        if (!Array.isArray(entries)) {
+            MyPromise.reject(new TypeError("You must pass an array to all."));
+        } else {
+            if (entries.length === 0) {
+                return MyPromise.resolve([]);
+            }
+            return new MyPromise((res, rej) => {
+                entries.forEach(e => {
+                    MyPromise.resolve(e).then((data: any) => {
+                        result.push(data);
+                        num += 1;
+                        if (num === entries.length) {
+                            res(result);
+                        }
+                    }, rej);
+                });
+            });
+        }
+    }
+    static race = (entries: []) => {
+        return new MyPromise((res, rej) => {
+            if (!Array.isArray(entries)) {
+                rej(new TypeError("You must pass an array to race."))
+            } else {
+                for (let e of entries) {
+                    MyPromise.resolve(e).then(res, rej);
+                }
+            }
+        });
     }
 };
 export default MyPromise;
